e96927a86ac9917a57cca030efb67cd5e2fbb935
Robin Böck <robin.boeck@tum.de>
2026-05-31 10:42:31 +0200

    Setup JWT authentication, context provider, apiFetch function, centralized paths config

---
+++ frontend/src/types/account.ts
@@ -0,0 +1,23 @@
+export interface Account {
+  accountId: string;
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  iban: string | null;
+  ibanHolderName: string | null;
+  deletedAt: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface LoginInput {
+  email: string;
+  password: string;
+}
+
+export interface SignupInput {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
